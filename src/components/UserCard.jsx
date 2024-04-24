6671706954aee938943748acea84aafef3eb8ddc
Beatriz Taborda <sgsstpymes2019@gmail.com>
2024-04-23 22:13:05 -0500

    entregale-4-react

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles/userCard.css';
+
+const UserCard = ({user, deleteUser, setUpdateUser}) => {
+
+
+    const handleDelete = () => {
+            deleteUser('users', user.id);
+    }
+
+    const handleEdit = () => {
+        setUpdateUser(user);
+    }
+
+  return (
+    <section className='user'>
+        <h2 className='user__name'>{user.first_name} {user.last_name}</h2>
+        <hr  className='user__line' />
+        <ul className='user__list'>
+            <li className='user__item'><span>Email: </span><span><ion-icon name="mail-outline"></ion-icon> {user.email}</span></li>
+            <li className='user__item'><span>Birthday: </span><span><ion-icon name="gift-outline"></ion-icon> {user.birthday}</span></li>
+        </ul>
+        <hr  className='user__line' />
+        <div className='user__buttons'>
+            <button className='user__btn' onClick={handleDelete}><ion-icon name="trash-outline"></ion-icon></button>
+            <button className='user__btn' onClick={handleEdit}><ion-icon name="create-outline"></ion-icon></button>
+        </div>
+    </section>
+  )
+}
+
+export default UserCard;
